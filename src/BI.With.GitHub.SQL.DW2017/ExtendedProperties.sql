d6de19fa964b91e06f648d247e494a4afce615cc
Olivier Delmotte <odeatavanade@users.noreply.github.com>
2021-08-26 19:22:02 +0200

    Sql Versionning

---
+++ src/BI.With.GitHub.SQL.DW2017/ExtendedProperties.sql
@@ -0,0 +1,8 @@
+﻿EXEC sp_addextendedproperty @name='BuildNumber', @value ='__GITHUB_RUN_NUMBER__', @level0type = NULL, @level0name = NULL, @level1type = NULL, @level1name = NULL, @level2type = NULL, @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name='BuildId', @value ='__GITHUB_RUN_ID__', @level0type = NULL, @level0name = NULL, @level1type = NULL, @level1name = NULL, @level2type = NULL, @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name='Repo', @value ='__GITHUB_REPOSITORY__', @level0type = NULL, @level0name = NULL, @level1type = NULL, @level1name = NULL, @level2type = NULL, @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name='Workflow', @value ='__GITHUB_WORKFLOW__', @level0type = NULL, @level0name = NULL, @level1type = NULL, @level1name = NULL, @level2type = NULL, @level2name = NULL
+GO
