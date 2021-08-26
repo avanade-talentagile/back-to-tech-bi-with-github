254f77d2734a5588a9c58af69b02e55dbefa4cac
Olivier Delmotte <odeatavanade@users.noreply.github.com>
2021-08-26 21:55:25 +0200

    bad edit

---
+++ src/BI.With.GitHub.NbiTests/BadEdit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BI.With.GitHub.NbiTests
+{
+    public class BadEdit
+    {
+        i'm pretty sure it won't compile :D
+    }
+}
